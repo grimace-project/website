ef0091025699c67963188418c6266e7f9d514ea1
Oliver Spindler <os@oliverspindler.com>
2024-03-26 16:09:24 +0100

    Add browser integration icons

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,37 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
 import { LinksFunction } from '@remix-run/node'
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 import stylesheet from '~/tailwind.css?url'
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/apple-touch-icon.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon-16x16.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/x-icon',
+    href: '/favicon.ico',
+  },
+  { rel: 'manifest', href: '/site.webmanifest' },
+  {
+    rel: 'mask-icon',
+    href: '/safari-pinned-tab.svg',
+    color: '#5bbad5',
+  },
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
